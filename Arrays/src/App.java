971140d854bb81e96bfb3d62dc5c2f19a63f06a2
Mark Pakingan <mrk.pkngn@gmail.com>
2025-10-07 13:12:04 +0200

    Arrays and int...

---
+++ Arrays/src/App.java
@@ -0,0 +1,70 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /* 
+        // Single-dimensional arrays
+         * int[] numbers = {10, 20, 30, 40, 50};
+        float[] values = new float[3];
+
+        System.out.println("First elemnt: " + numbers[0]);
+        numbers[2] = 35;
+        values[1] = 3.14f;
+
+        System.out.println("Array of nubers elements: ");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("Array of values elements: ");
+        for(float value: values) {
+            System.out.print(value + " ");
+        }
+
+        // Multidimensional arrays
+        int[][] matrix = new int[3][3];
+        matrix[0][0] = 1;
+        matrix[1][1] = 5;
+
+        for(int x = 0; x < matrix.length; x++){
+            for(int y = 0; y < matrix[x].length; y++){
+                System.out.print(matrix[x][y] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int[] rows : matrix){
+            for(int col : rows){
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+
+        // int...
+        printNumbers(1, 2, 3, 4, 5);
+        System.out.println();
+        int[] numbers = {1, 2, 3, 4, 5};
+        printOldFashioned(numbers);
+        */
+        
+        
+
+    }
+
+    /*
+     public static void printNumbers(int... numbers){
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+
+    public static void printOldFashioned(int[] numbers){
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+     */
+    
+}
