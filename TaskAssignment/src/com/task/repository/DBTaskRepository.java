a9b18bac4ccf2962f7e91626618f62e678acfab5
Mark Pakingan <mrk.pkngn@gmail.com>
2025-12-04 14:09:58 +0100

    Added PreparedStatement

--- TaskAssignment/src/com/task/repository/DBTaskRepository.java
+++ TaskAssignment/src/com/task/repository/DBTaskRepository.java
@@ -1,10 +1,33 @@
 package com.task.repository;
 
+import java.io.IOException;
 import java.util.Vector;
-
 import com.task.model.TaskItem;
+import com.task.model.TaskStatus;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 
 public class DBTaskRepository implements TaskRepository{
+
+    private Vector<TaskItem> _taskItems;
+    private int _totalTaskItems;
+    private String _url = "jdbc:mysql://vsrvfeia0h-64.vsb.cz:3306/user_tasks_db";
+    private String _username = "guest";
+    private String _password = "guest_password"; 
+    private Connection _connection;
+
+    public DBTaskRepository() {
+        this._taskItems = new Vector<TaskItem>();
+        try {
+            _connection = DriverManager.getConnection(_url, _username, _password);
+            PreparedStatement stmt = _connection.prepareStatement("");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+     }
 
     @Override
     public TaskItem findById(int id) {
